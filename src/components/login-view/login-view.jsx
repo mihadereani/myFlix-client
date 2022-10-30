cc0b1663a6ebb0dce28893ec525964d77c3bb44f
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-10-30 14:40:49 +0100

    create login form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -11,26 +13,24 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input
+    <Form>
+      <Form.Group controlId='formUsername'>
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
           type='text'
-          value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-      </label>
-      <label>
-        Password:
-        <input
+      </Form.Group>
+      <Form.Group controlId='formPassword'>
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
           type='password'
-          value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-      </label>
-      <button type='submit' onClick={handleSubmit}>
-        Log in
-      </button>
-    </form>
+      </Form.Group>
+      <Button variant='primary' type='submit' onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
   );
 }
