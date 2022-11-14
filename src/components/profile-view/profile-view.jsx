4d2ec08f042ceaad21aeb91101427dd2d2ce4720
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-11-14 19:40:10 +0100

    create favorite-movies

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -11,7 +11,8 @@ import {
 } from 'react-bootstrap';
 
 import './profile-view.scss';
-import { Link } from 'react-router-dom';
+
+import { FavoriteMovies } from '../favorite-movies/favorite-movies';
 
 export function ProfileView({ movies }) {
   const [user, setUser] = useState('');
@@ -214,54 +215,7 @@ export function ProfileView({ movies }) {
           </Card>
         </Col>
       </Row>
-
-      <Row className='mt-5'>
-        <Col>
-          <Card.Title className='mb-2'>Favorite Movies</Card.Title>
-
-          <Row>
-            {favoriteMovies.map((m) => {
-              return (
-                <Col className='fav-movie' xs={12} md={6} lg={3} key={m._id}>
-                  <Figure>
-                    <Link to={`/movies/${m._id}`}>
-                      <Figure.Image
-                        variant='top'
-                        src={m.ImagePath}
-                        alt={m.Title}
-                      />
-                      <Figure.Caption>{m.Title}</Figure.Caption>
-                    </Link>
-                  </Figure>
-                  <Button
-                    variant='secondary'
-                    type='submit'
-                    onClick={(e) => {
-                      e.preventDefault();
-                      axios
-                        .delete(
-                          `https://myflixmiha.herokuapp.com/users/${currentUser}/movies/${m._id}`,
-                          {
-                            headers: { Authorization: `Bearer ${token}` },
-                          }
-                        )
-                        .then((response) => {
-                          alert('Favorite movie has been removed.');
-                        })
-                        .catch((error) => {
-                          console.error(error);
-                          alert('Unable to remove favorite movie.');
-                        });
-                    }}
-                  >
-                    Remove from list
-                  </Button>
-                </Col>
-              );
-            })}
-          </Row>
-        </Col>
-      </Row>
+      <FavoriteMovies favoriteMovies={favoriteMovies} />
     </Container>
   );
 }
