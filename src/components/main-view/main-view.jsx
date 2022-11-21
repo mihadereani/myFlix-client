ee9d1a7ad317f3011992dd182ec8d689632a1a6f
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-11-21 18:56:50 +0100

    create store

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
+
 import { NavBar } from '../navbar/navbar';
 
 import {
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import moviesApp from './reducers/reducers';
+
 import { MainView } from './components/main-view/main-view';
 
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container className='my-flix'>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container className='my-flix'>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
